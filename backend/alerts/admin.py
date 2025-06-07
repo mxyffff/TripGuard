7c772b477fbb9e393e589e98a8d1b4c19c1b4390
unknown <boyunyang@naver.com>
2025-06-07 17:25:09 +0900

    r

--- backend/alerts/admin.py
+++ backend/alerts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import TravelAlert
+
+@admin.register(TravelAlert)
+class TravelAlertAdmin(admin.ModelAdmin):
+    list_display = ('country_name_kr', 'alarm_level', 'written_date')
+    list_filter = ('alarm_level', 'continent_name_kr')
+    search_fields = ('country_name_kr', 'country_iso_alpha2')
+
